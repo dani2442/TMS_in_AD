54a9f5e52ffab97b8e18b3eef4e482fc0393ef13
Riccardo Leone <riccardoleone1991@gmail.com>
2023-03-24 16:01:24 +0100

    Modify adnimerge + analysis

--- 01_calculate_WMH_load/extract_WMH_volumes.py
+++ 01_calculate_WMH_load/extract_WMH_volumes.py
@@ -11,8 +11,8 @@ Comments:
 
 Sources:  
 """
-# %% Imports and directories
-import pickle
+# %%
+# Imports and directories
 import numpy as np
 import pandas as pd
 import nibabel as nib
@@ -20,7 +20,6 @@ import nibabel.imagestats as nibstats
 from bids import BIDSLayout
 from pathlib import Path
 
-# Directories
 SPINE = Path.cwd().parents[2]
 DATA_DIR = SPINE / "data"
 PREP_DIR = DATA_DIR / "preprocessed"
@@ -36,58 +35,65 @@ WMH_RES_DIR = RES_DIR / "WMH_lesion_load"
 if not Path.is_dir(WMH_RES_DIR):
     Path.mkdir(WMH_RES_DIR)
 
-# Define useful functions 
-def calculate_WMH_load(subj):
-    wmh_mask = layout.get(
-        subject=subj, label="WMHMask", return_type="file"
-    )
+# Define useful functions
+def calculate_WMH_load_subj_space(subj):
+    wmh_mask = layout.get(subject=subj, label="WMHMask", return_type="file")
     wmh_mask = [f for f in wmh_mask if "space" not in f]
-    if wmh_mask != list():
+    try:
         wmh = nib.load(wmh_mask[0])
         wmh_volume = np.round(nibstats.mask_volume(wmh), 1)
-    else:
+    except:
         wmh_volume = 0
     return wmh_volume
 
+#%%
+def calculate_WMH_load_mni_space(subj):
+    wmh_mask_mni = WMH_DIR / f"sub-{subj}" / f"sub-{subj}_ses-M00_space-MNI152NLin6Asym_desc-fromSubjSpace.nii.gz"
+    try:
+        wmh_mni = nib.load(wmh_mask_mni)
+        wmh_volume_mni = np.round(nibstats.mask_volume(wmh_mni), 1)
+    except:
+        print(f"MNI mask not found for subj-{subj}")
+        wmh_volume_mni = 0    
+    return wmh_volume_mni
+
+#%%
 # Get the BIDS layout and subject list
-print('Loading the folder layout')
+print("Loading the folder layout...")
 layout = BIDSLayout(WMH_DIR, validate=False, config=["bids", "derivatives"])
 subjs = layout.get_subjects()
-
-# iterate through subjects, since not all subjects have WMH masks, perform WMH volume extraction only on those who have the WMH segmentation masks
-global_WMH_dict = {}
-
-for i, subj in enumerate(subjs):
-    print(f'Processing sub-{subj}... ({i+1}/{len(subjs)})')
-    global_WMH_dict[subj] = calculate_WMH_load(subj)
-
-WMH_subjs_list = []
-WMH_burden_list = []
-
-for k, v in global_WMH_dict.items():
-    WMH_subjs_list.append(k)
-    WMH_burden_list.append(v)
-
-WMH_burden_arr = np.array(WMH_burden_list)
-
+#%%
+# Calculate the WMH lesion load for each subj and store in pandas df
+global_WMH_df = pd.DataFrame()
+global_WMH_df["PTID"] = subjs
+#%%
+print("Calculating WMH lesion load in subject space...")
+global_WMH_df["WMH_load_subj_space"] = [
+    calculate_WMH_load_subj_space(subj) for subj in global_WMH_df["PTID"]
+]
+# %%
+print("Calculating WMH lesion load in mni space...")
+global_WMH_df["WMH_load_mni_space"] = [
+    calculate_WMH_load_mni_space(subj) for subj in global_WMH_df["PTID"]
+]
+print("Done with WMH lesion load")
+#%%
 # Normalize the whole group in [0,1]
-print('Normalizing the WMH burden across all subjects...')
-normalized_WMH_burden_series = np.round(
-    (WMH_burden_arr - np.min(WMH_burden_arr)) / np.ptp(WMH_burden_arr), 5
-)  
+print("Normalizing the WMH burden across all subjects in subject space...")
+global_WMH_df["WMH_load_norm_subj_space"] = np.round(
+    (global_WMH_df["WMH_load_subj_space"] - global_WMH_df["WMH_load_subj_space"].min())
+    / np.ptp(global_WMH_df["WMH_load_subj_space"]),
+    5,
+)
 
-global_WMH_dict_normalized = {
-    k: normalized_WMH_burden_series[n] for n, k in enumerate(WMH_subjs_list)
-}
+print("Normalizing the WMH burden across all subjects in MNI space...")
+global_WMH_df["WMH_load_norm_mni_space"] = np.round(
+    (
+        global_WMH_df["WMH_load_mni_space"]
+        - global_WMH_df["WMH_load_mni_space"].min()
+    )
+    / np.ptp(global_WMH_df["WMH_load_mni_space"]),
+    5,
+)
 
-# Save global WMH load dict, first open file for writing, "w"
-print(f'Saving dictionaries in {WMH_RES_DIR}...')
-g = open(WMH_RES_DIR / "global_WMH_burden_all_subjs.pkl", "wb")
-# write json object to file
-pickle.dump(global_WMH_dict, g)
-# close file
-g.close()
-# Save normalized dict
-f = open(WMH_RES_DIR / "global_WMH_burden_all_subjs_normalized.pkl", "wb")
-pickle.dump(global_WMH_dict_normalized, f)
-f.close()
+# %%
