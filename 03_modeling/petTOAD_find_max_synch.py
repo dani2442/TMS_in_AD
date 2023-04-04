e36b8d2f7c94f9412636b9b757780b463e96ddb0
Riccardo Leone <riccardoleone1991@gmail.com>
2023-04-04 15:12:08 +0200

    Trying class npencoder for json non serializable

--- 03_modeling/petTOAD_find_max_synch.py
+++ 03_modeling/petTOAD_find_max_synch.py
@@ -83,7 +83,7 @@ if not Path.is_dir(HC_DIR):
 outFilePath = str(HC_DIR)
 
 # %%
-a_s = np.round(np.arange(-0.020, 0.000, 0.001), 3)
+a_s = np.round(np.arange(-0.020, 0.000, 0.002), 3)
 synch_vals = np.round(np.arange(0.1, 0.21, 0.01), 3)
 
 opt_dict = {}
