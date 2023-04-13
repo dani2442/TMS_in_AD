Fixed error in wmh normalization 0-1 range

03_modeling/petTOAD_final_model.py
@@ -24,7 +24,7 @@ conditionToStudy = "hc"  # one of 'hc', 'mci', 'all'
 mode = "homogeneous"  # one of 'homogeneous', 'heterogeneous_sc', 'heterogeneous_node'
 random = False  # set to True if you want to shuffle the wmh weights
 
-wmh_dict = get_wmh_load_homogeneous()
+wmh_dict = get_wmh_load_homogeneous(subjs)
 
 if conditionToStudy == "hc":
     all_fMRI = {k: v for k, v in all_fMRI.items() if k in HC_WMH}
@@ -105,7 +105,7 @@ def fittingPipeline_homogeneous(
     # Now, evaluate different bifurcation parameters depending on WMH burden
     wmParms = [
         {"a": base_a_value + (wmW * wmh_burden) + 0.002} for wmW in wmWs
-    ]  # need to set up b
+    ]  
     fitting = ParmSeep.distanceForAll_Parms(
         subj_fMRI,
         wmWs,
@@ -127,7 +127,7 @@ def fittingPipeline_homogeneous(
 Hopf.setParms({"we": 0.33})
 
 # Set the weights for all simulations:
-wmWs = np.round(np.arange(-0.101, 0.101, 0.001), 3)
+wmWs = np.round(np.arange(-0.100, 0.100, 0.0025), 3)
 
 def fittingPipeline_heterogeneous(all_fMRI, wmh_burden_dict, wmWs):
     best_parameters_dict = {}

03_modeling/petTOAD_load.py
@@ -154,10 +154,11 @@ def get_classification(subjs):
 
 
 # %%
-def get_wmh_load_homogeneous():
+def get_wmh_load_homogeneous(subjs):
     adnimerge = pd.read_csv(RES_DIR / "petTOAD_dataframe.csv")
     adnimerge["PTID"] = adnimerge["PTID"].str.replace("_", "")
-    adnimerge["WMH_load_subj_space_norm"] = adnimerge['WMH_load_subj_space'] / adnimerge['WMH_load_subj_space'].max()
+    adnimerge = adnimerge[adnimerge['PTID'].isin(subjs)]
+    adnimerge["WMH_load_subj_space_norm"] = (adnimerge['WMH_load_subj_space'] - adnimerge['WMH_load_subj_space'].min()) / (adnimerge['WMH_load_subj_space'].max() - adnimerge['WMH_load_subj_space'].min())
     homo_wmh_dict = dict(zip(adnimerge['PTID'], round(adnimerge['WMH_load_subj_space_norm'], 3)))
     return homo_wmh_dict
 
@@ -173,11 +174,3 @@ def get_node_damage(subj):
     # Computes the percent damage sustained by each node
     wmh_damage = wmh_df.iloc[:200, 1:201].mean(axis=0).to_numpy()
     return wmh_damage
-
-def get_wmh_load_homogeneous():
-    adnimerge = pd.read_csv(RES_DIR / "petTOAD_dataframe.csv")
-    adnimerge["PTID"] = adnimerge["PTID"].str.replace("_", "")
-    adnimerge["WMH_load_subj_space_norm"] = adnimerge['WMH_load_subj_space'] / adnimerge['WMH_load_subj_space'].max()
-    homo_wmh_dict = dict(zip(adnimerge['PTID'], round(adnimerge['WMH_load_subj_space_norm'], 3)))
-    return homo_wmh_dict
-# %%

03_modeling/petTOAD_setup.py
@@ -99,8 +99,9 @@ sc_norm = sc * 0.2 / sc.max()
 all_fMRI = {}
 for subj in subjs:
     all_fMRI[subj] = get_method_ts(subj)
-# Now we have different types of preprocessing with XCP_D, choose only one for trying out
 all_fMRI = checking_timeseries(all_fMRI)
+# New subject list overwrites the old one
+subjs = [k for k in all_fMRI.keys()]
 HC_no_WMH, HC_WMH, MCI_no_WMH, MCI_WMH = get_classification(subjs)
 MCI = np.array([j for i in [MCI_WMH, MCI_no_WMH] for j in i]).astype('object')
 all_HC_fMRI = {k: v for k, v in all_fMRI.items() if k in HC_no_WMH}