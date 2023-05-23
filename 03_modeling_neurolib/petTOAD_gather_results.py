5adceaf0c94af328bf975eabf2e64be240c0a3a2
Riccardo Leone <riccardoleone1991@gmail.com>
2023-05-23 14:48:13 +0200

    fixed merge conflicts

--- 03_modeling_neurolib/petTOAD_gather_results.py
+++ 03_modeling_neurolib/petTOAD_gather_results.py
@@ -85,24 +85,6 @@ nparms = len(sim.parameters.K_gl)
 bold_arr = get_bolds_from_trajs(trajs).T
 fc_array, fcd_array, phfcd_array = calculate_results_from_bolds(bold_arr)
 sim_fc = fc_array.mean(axis=1)
-<<<<<<< HEAD
-<<<<<<< HEAD
-import time
-
-fc_pearson = [func.matrix_correlation(row_fc, fc) for row_fc in sim_fc]
-start_time = time.time()
-fcd_ks = [my_func.matrix_kolmogorov(fcd, np.concatenate(row)) for row in fcd_array]
-end_time = time.time()
-print(f'It took {end_time-start_time} for 2 simulations of fcd')
-phfcd_ks = [
-    my_func.matrix_kolmogorov(phfcd, np.concatenate(row)) for row in phfcd_array
-]
-end_end_time = time.time()
-print(f'It took {end_end_time-end_time} for 2 simulations of phfcd')
-
-=======
-=======
->>>>>>> dacedf6623d86f928ff99cb57ea495339bc9a8bf
 print("Calculating fcs...")
 start_time = time.time()
 fc_pearson = [func.matrix_correlation(row_fc, fc) for row_fc in sim_fc]
@@ -118,10 +100,6 @@ phfcd_ks = [
 ]
 real_end_time = time.time()
 print(f"It took {round(real_end_time - end_end_time, 3) / 60} mins to process phFCDs")
-<<<<<<< HEAD
->>>>>>> dacedf6623d86f928ff99cb57ea495339bc9a8bf
-=======
->>>>>>> dacedf6623d86f928ff99cb57ea495339bc9a8bf
 data = [[sim.parameters.K_gl, fc_pearson, fcd_ks, phfcd_ks]]
 columns = ["K_gl", "fc_pearson", "fcd_ks", "phfcd_ks"]
 res_df = pd.DataFrame(data, columns=columns).explode(columns)
