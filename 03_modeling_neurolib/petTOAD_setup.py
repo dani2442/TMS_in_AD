4e51341690742761dba8b62800dfb2308574aac9
Riccardo Leone <riccardoleone1991@gmail.com>
2023-08-29 12:01:33 +0200

    final setup

--- 03_modeling_neurolib/petTOAD_setup.py
+++ 03_modeling_neurolib/petTOAD_setup.py
@@ -14,8 +14,10 @@ Sources: filtPowSpectr abd BOLDFilters taken from Gustavo Patow's WholeBrain Cod
 # %%
 # Imports
 import numpy as np
-import BOLDFilters as BOLDFilters
 from scipy import stats
+from scipy.signal import detrend
+
+import BOLDFilters as BOLDFilters
 from petTOAD_load import *
 
 # Get subjs names
@@ -33,15 +35,16 @@ n_nodes = sc.shape[0]
 all_fMRI_raw = {subj: load_ts_aal(subj) for subj in subjs}
 # Check that the timeseries do not have regions with all zeros and return only OK subjs
 all_fMRI_raw = check_ts(all_fMRI_raw)
-# Demean, detrend and filter the signal only for subjects with complete data
+# Filter, detrend, z-score the signal only for subjects with complete data
 all_fMRI_clean = {
-    subj: stats.zscore(BOLDFilters.BandPassFilter(sub_ts), axis=1)
+    subj: stats.zscore(detrend(BOLDFilters.BandPassFilter(sub_ts)), axis=1)
     for subj, sub_ts in all_fMRI_raw.items()
 }
 # New subject list overwrites the old one
 subjs = [k for k in all_fMRI_clean.keys()]
-# Get subject list for condition
-HC, MCI, HC_no_WMH, HC_WMH, MCI_no_WMH, MCI_WMH = get_classification(subjs)
+# Get subject list for each condition
+CN, MCI, CN_no_WMH, CN_WMH, MCI_no_WMH, MCI_WMH = get_classification(subjs)
 print("petTOAD Setup done!")
 
-# %%
+# We only want to simulate with WMH-weighted models those patients that do have wmh
+subjs_to_sim = np.hstack([CN_WMH, MCI_WMH])
